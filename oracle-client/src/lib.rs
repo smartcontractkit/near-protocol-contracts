706badb20a7616a87480c9113762742ec72a55ba
Mike Purvis <mikedotexe@gmail.com>
2020-06-09 16:43:39 -0700

    use Base64 crate, improved params, cleanup, etc for oracle-client

--- oracle-client/src/lib.rs
+++ oracle-client/src/lib.rs
@@ -1,9 +1,12 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
+use base64::{decode};
 use std::str;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+pub type Base64String = String;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
@@ -11,18 +14,17 @@ struct ClientContract {}
 
 #[near_bindgen]
 impl ClientContract {
-    // #[init]
-    // pub fn new() -> Self {
-    //     // useful snippet to copy/paste, making sure state isn't already initialized
-    //     assert!(env::state_read::<Self>().is_none(), "Already initialized");
-    //     Self {}
-    // }
-
-    pub fn token_price_callback(&mut self, price: Vec<u8>) {
-        let price_as_string = match str::from_utf8(price.as_slice()) {
+    #[allow(dead_code)] // This function gets called from the oracle
+    pub fn token_price_callback(&mut self, price: Base64String) {
+        let base64_price = match str::from_utf8(price.as_bytes()) {
             Ok(val) => val,
             Err(_) => env::panic(b"Invalid UTF-8 sequence provided from oracle contract."),
         };
-        env::log(format!("Client contract received price: {}", price_as_string).as_bytes());
+        let decoded_price_vec = decode(base64_price).unwrap();
+        let price_readable = match str::from_utf8(decoded_price_vec.as_slice()) {
+            Ok(val) => val,
+            Err(_) => env::panic(b"Invalid UTF-8 sequence in Base64 decoded value."),
+        };
+        env::log(format!("Client contract received price: {:?}", price_readable).as_bytes());
     }
 }
