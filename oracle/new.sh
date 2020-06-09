42eeba6e8ee9b584bf3b479f77851ade3ff9b9d8
Mike Purvis <mikedotexe@gmail.com>
2020-06-09 08:35:57 -0700

    modify new script to add link_id and remove withdrawable_tokens

--- oracle/new.sh
+++ oracle/new.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-near call v0.oracle.testnet new '{"owner_id": "v0.oracle.testnet", "withdrawable_tokens": "1000000"}' --accountId v0.oracle.testnet
+near call v0.oracle.testnet new '{"link_id": "v0.link.testnet", "owner_id": "v0.oracle.testnet"}' --accountId v0.oracle.testnet
