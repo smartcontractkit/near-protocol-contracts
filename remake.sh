c8dd2c34adf774a92c6dda735dbad29457cdec38
Mike Purvis <mikedotexe@gmail.com>
2020-06-25 16:03:12 -0700

    remake.sh: change oracle-client to client

--- remake.sh
+++ remake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 near delete oracle.$NEAR_ACCT $NEAR_ACCT
 near create_account oracle.$NEAR_ACCT --masterAccount $NEAR_ACCT
-near delete oracle-client.$NEAR_ACCT $NEAR_ACCT
-near create_account oracle-client.$NEAR_ACCT --masterAccount $NEAR_ACCT
+near delete client.$NEAR_ACCT $NEAR_ACCT
+near create_account client.$NEAR_ACCT --masterAccount $NEAR_ACCT
 near delete oracle-node.$NEAR_ACCT $NEAR_ACCT
 near create_account oracle-node.$NEAR_ACCT --masterAccount $NEAR_ACCT
 near delete near-link.$NEAR_ACCT $NEAR_ACCT
