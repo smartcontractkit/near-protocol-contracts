51460ca23207956634841fb305ea7f00c7a2d69d
Mike Purvis <mikedotexe@gmail.com>
2020-07-27 20:57:11 -0700

    fix the remake script to use create-account instead of old way

--- remake.sh
+++ remake.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 near delete oracle.$NEAR_ACCT $NEAR_ACCT
-near create_account oracle.$NEAR_ACCT --masterAccount $NEAR_ACCT
+near create-account oracle.$NEAR_ACCT --masterAccount $NEAR_ACCT
 near delete client.$NEAR_ACCT $NEAR_ACCT
-near create_account client.$NEAR_ACCT --masterAccount $NEAR_ACCT
+near create-account client.$NEAR_ACCT --masterAccount $NEAR_ACCT
 near delete oracle-node.$NEAR_ACCT $NEAR_ACCT
-near create_account oracle-node.$NEAR_ACCT --masterAccount $NEAR_ACCT
+near create-account oracle-node.$NEAR_ACCT --masterAccount $NEAR_ACCT
 near delete near-link.$NEAR_ACCT $NEAR_ACCT
-near create_account near-link.$NEAR_ACCT --masterAccount $NEAR_ACCT
+near create-account near-link.$NEAR_ACCT --masterAccount $NEAR_ACCT
